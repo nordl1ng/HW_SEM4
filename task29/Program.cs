5b4c1496b8b20395840a7d07e416efa3fb2c508f
DmitryZ <zagarskih@inbox.ru>
2022-07-06 18:52:49 +0500

    добавил ввод количества элементов массива

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,18 +2,19 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-System.Console.WriteLine("Введите числа массива");
-int [] array = new int [8];
-for (int i =0; i < 8; i ++)
+System.Console.WriteLine("Введите количество элементов массива");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("ВВЕДИТЕ ЧИСЛА МАССИВА");
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
 {
     System.Console.WriteLine($"Введите число для элемента {i}:");
-    array [i]= int.Parse(Console.ReadLine()!);
+    array[i] = int.Parse(Console.ReadLine()!);
 }
-// for (int i =0; i < 8; i ++)
 System.Console.Write("[");
 foreach (int i in array)
 {
-System.Console.Write("{0} ",i);
+    System.Console.Write("{0} ", i);
 }
 System.Console.Write("\b \b");
 System.Console.Write("]");
